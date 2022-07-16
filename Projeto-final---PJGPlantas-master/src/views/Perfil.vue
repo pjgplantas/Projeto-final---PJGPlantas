fcc7a61a1390ef4b56e7249ae721f5d19ee4de76
pjgplantas <97185869+pjgplantas@users.noreply.github.com>
2022-07-16 14:15:37 -0300

    Add files via upload

---
+++ Projeto-final---PJGPlantas-master/src/views/Perfil.vue
@@ -0,0 +1,295 @@
+<template>
+  <div>
+    <b-container class="login-page" fluid>
+      <b-row>
+        <b-col>
+          <b-col>
+            <b-form
+              class="formp"
+              @submit.prevent="login"
+              @reset="onReset"
+              v-if="show"
+            >
+              <div class="parte1">
+                <div class="imgperfil">
+                  <img
+                    class="rounded-circle mt-5"
+                    width="150px"
+                    src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                  />
+                </div>
+                <div class="NomeCliente">João</div>
+                <div class="EmailCliente">jaumm.silva.04@gmail.com</div>
+              </div>
+              <div class="parte2"><hr class="HrForm" /></div>
+              <div class="parte3">
+                <div class="TituloPerfil">Configurações do Perfil</div>
+                <div class="divs">Email</div>
+                <b-form-group id="input-group-2">
+                  <b-form-input id="input-2" v-model="form.email" required>
+                  </b-form-input>
+                </b-form-group>
+                <div class="divs">Nome:</div>
+                <b-form-group id="input-group-2">
+                  <b-form-input
+                    id="input-2"
+                    type="name"
+                    v-model="form.name"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+                <div class="divs">Endereço:</div>
+                <b-form-group id="input-group-2">
+                  <b-form-input
+                    id="input-2"
+                    v-model="form.endereco"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+                <div class="divs">Data de nascimento:</div>
+                <b-form-group id="input-group-2">
+                  <b-form-input
+                    id="input-2"
+                    type="date"
+                    v-model="form.date"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+                <div class="divs">Sexo:</div>
+                <b-form-group class="forms" v-slot="{ ariaDescribedby }">
+                  <b-form-radio
+                    id="opt1"
+                    v-model="selected"
+                    :aria-describedby="ariaDescribedby"
+                    name="some-radios"
+                    value="A"
+                    >Feminino</b-form-radio
+                  >
+                  <b-form-radio
+                    id="opt2"
+                    v-model="selected"
+                    :aria-describedby="ariaDescribedby"
+                    name="some-radios"
+                    value="B"
+                    >Masculino</b-form-radio
+                  >
+                </b-form-group>
+                <b-button class="btncad" type="submit">Salvar perfil</b-button>
+              </div>
+            </b-form>
+          </b-col>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "perfil",
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        selected: "",
+        endereco: "",
+        date: "",
+      },
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.date = "";
+      this.form.selected = " ";
+      this.form.endereco = "";
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+.formp {
+  border-radius: 2px;
+  height: 98vh;
+  width: 173vh;
+  margin: 2% 0 0 5%;
+  background-color: white;
+  display: flex;
+}
+.login-page {
+  width: 1950px;
+  height: 1000px;
+  background: #00bf8f; /* fallback for old browsers */
+  background: -webkit-(
+    to top,
+    #001510,
+    #00bf8f
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: #00bf8f; /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+.parte1 {
+  margin: 2% 0 0 15%;
+}
+.NomeCliente {
+  font-family: "Roboto", sans-serif;
+  font-size: 25px;
+  font-weight: 600;
+  margin: 0 0% 0 24%;
+}
+
+.EmailCliente {
+  color: black;
+  opacity: 50%;
+  font-size: 19px;
+}
+.HrForm {
+  border: 1px solid #808080;
+  height: 88vh;
+}
+.parte2 {
+  margin: 3% 0 0 17%;
+}
+.parte3 {
+  margin: 5% 0 0 10%;
+}
+.TituloPerfil {
+  font-family: "Roboto", sans-serif;
+  font-size: 2rem;
+  color: black;
+  padding: 0px;
+  text-align: center;
+}
+.divs {
+  color: black;
+  font-style: normal;
+  font-weight: 500;
+  white-space: nowrap;
+  font-size: 19px;
+  margin: 10% 0 0 0;
+}
+.form-control {
+  background: white;
+  border-style: none none solid none;
+  border-color: rgba(62, 150, 97, 0.95);
+  width: 480px;
+}
+.btncad {
+  background: rgb(21, 213, 165);
+  background: linear-gradient(
+    7deg,
+    rgba(21, 213, 165, 1) 0%,
+    rgba(54, 131, 100, 1) 66%
+  );
+  border: 0;
+  padding: 13px;
+  width: 150px;
+  font-size: 23px;
+  border-radius: 10px;
+  margin: 10% 0 0 32%;
+}
+
+@media (max-width: 844px) {
+  .imgperfil {
+    margin-left: 25%;
+  }
+
+  .login-page {
+    width: 390px;
+    height: 1230px;
+    padding: 0;
+  }
+  .formp {
+    width: 370px;
+    flex-direction: column;
+    height: 1200px;
+    margin-left: 6%;
+  }
+  .parte1 {
+    width: 300px;
+    height: 380px;
+    justify-content: center;
+    margin-left: 30px;
+  }
+  .NomeCliente {
+    text-align: center;
+    margin: 0px;
+  }
+  .EmailCliente {
+    text-align: center;
+    margin: 0px;
+  }
+
+  .parte3 {
+    width: 320px;
+    height: 700px;
+    margin-left: 30px;
+    font-size: 10px;
+  }
+  .Titulo2Perfil {
+    font-size: 20px;
+  }
+  .divs {
+    font-size: 18px;
+  }
+  .form-control {
+    width: 320px;
+  }
+
+  .parte5 {
+    width: 300px;
+    height: 400px;
+    margin: 10% 0% 10% 8%;
+  }
+  .col {
+    padding: 0px;
+    margin: 0px;
+    width: 390px;
+  }
+  .parte2 {
+    display: none;
+  }
+  .parte4 {
+    display: none;
+  }
+
+  .hrlay {
+    display: none;
+  }
+  #footer {
+    color: #3e9661;
+    font-size: 16px;
+    margin: 10% 10% 7% 5%;
+    width: 340px;
+  }
+  .footer1 {
+    margin: 3%;
+    width: 250px;
+    padding: 5%;
+  }
+  .footer2 {
+    margin: 3%;
+    width: 250px;
+    padding: 5%;
+  }
+  .TituloPerfil {
+    font-size: 20px;
+  }
+}
+</style>
---
+++ Projeto-final---PJGPlantas-master/src/views/Produto.vue
@@ -0,0 +1,384 @@
+<template>
+  <div class="pageTotal">
+    <b-container fluid class="produto">
+      <b-row class="row">
+        <b-col class="col1">
+          <img
+            src="https://http2.mlstatic.com/D_NQ_NP_963647-MLB48382302651_112021-W.webp"
+            alt=""
+            class="img"
+          />
+        </b-col>
+        <b-col class="col2">
+          <h1><div class="nome">Nome do Produto</div></h1>
+          <br />
+          <br />
+          <h4>
+            <div class="descricao">
+              Produto de não sei oq la entendeu e simplesmente é isso. silencio
+              e trabalho, e não se esqueça... FORGET!
+            </div>
+          </h4>
+          <div class="subdescricao">
+            Por R$50,00 a vista <br />
+            ou 3x de R$ 19,50. <br />
+            <p></p>
+            Quantidade:
+          </div>
+          <div class="spinb">
+            <b-form-spinbutton
+              id="demo-sb"
+              v-model="value"
+              min="1"
+              max="10"
+              inline
+            ></b-form-spinbutton>
+          </div>
+          <div class="Total">
+            <h3>TOTAL:</h3>
+          </div>
+          <br />
+          <b-button class="btncad" type="submit" href="/dadoscompra"
+            >Finalizar</b-button
+          >
+        </b-col>
+      </b-row>
+      <div class="Comentarios">
+        <h1><div class="TituloComentario">Comentários</div></h1>
+        <div class="FazerComentario">
+          <b-form-textarea
+            class="TextoComentario"
+            id="textarea-formatter"
+            max-rows="7"
+            style="height: 160px"
+          >
+          </b-form-textarea>
+          <b-button class="BotaoComentario" type="submit">Comentar</b-button>
+        </div>
+        <div class="Respostas">
+          <b-avatar icon="people-fill" size="4em"></b-avatar>
+          <div class="RespostasCliente">
+            <div class="aResposta">
+              Nulla molestie, arcu eget condimentum hendrerit, turpis nisl
+              consectetur lorem, ut porta velit nulla sit amet erat. Maecenas
+              euismod dolor nec orci bibendum, egestas consequat orci accumsan.
+              Maecenas egestas sed dui ac fermentum. Phasellus vel rhoncus
+              lacus, sit amet facilisis massa.
+            </div>
+          </div>
+          <b-button class="BotaoComentario2" type="submit">Responder</b-button>
+        </div>
+        <div class="aResposta2">Respostas</div>
+        <div class="Respostas2">
+          <b-avatar class="avatar" icon="people-fill" size="3em"></b-avatar>
+          <div class="RespostasCliente2">
+            <div class="aResposta3">
+              Nulla molestie, arcu eget condimentum hendrerit, turpis nisl
+              consectetur lorem, ut porta velit nulla sit amet.
+            </div>
+          </div>
+          <b-icon class="icon" icon="heart-fill" scale="1"></b-icon>
+        </div>
+        <div class="Respostas2">
+          <b-avatar class="avatar" icon="people-fill" size="3em"></b-avatar>
+          <div class="RespostasCliente2">
+            <div class="aResposta3">
+              Nulla molestie, arcu eget condimentum hendrerit, turpis nisl
+              consectetur lorem, ut porta velit nulla sit amet.
+            </div>
+          </div>
+          <b-icon class="icon" icon="heart-fill" scale="1"></b-icon>
+        </div>
+        <div class="Respostas2">
+          <b-avatar class="avatar" icon="people-fill" size="3em"></b-avatar>
+          <div class="RespostasCliente2">
+            <div class="aResposta3">
+              Nulla molestie, arcu eget condimentum hendrerit, turpis nisl
+              consectetur lorem, ut porta velit nulla sit amet.
+            </div>
+          </div>
+          <b-icon class="icon" icon="heart-fill" scale="1"></b-icon>
+        </div>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 1,
+    };
+  },
+};
+</script>
+
+<style>
+.pageTotal {
+  background: #00bf8f; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to top,
+    #001510,
+    #00bf8f
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to top,
+    #001510,
+    #00bf8f
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  height: 1950px;
+}
+.row {
+  background: #ffffff;
+  border-radius: 5px;
+}
+.produto {
+  padding: 30px;
+  font-family: "Jomolhari", serif;
+  height: 100vh;
+}
+.col1 {
+  border-radius: 4px;
+  padding: 1px;
+  margin-left: 5px;
+  box-shadow: 5px 8px 7px 6px rgba(15, 52, 6, 0.85);
+}
+.col2 {
+  border-radius: 4px;
+  box-shadow: 5px 8px 7px 6px rgba(15, 52, 6, 0.85);
+}
+.nome {
+  margin-top: 170px;
+  color: #0f3406;
+  text-align: center;
+}
+.descricao {
+  text-align: left;
+  color: #0f3406;
+  padding: 20px;
+  margin: 0 0 0 10%;
+}
+.subdescricao {
+  text-align: left;
+  color: #3e9661;
+  opacity: 80%;
+  font-size: 22px;
+  padding: 18px;
+  margin: 0 0 0 10%;
+}
+.Total {
+  margin: 0 0 0 12%;
+  padding: 10px;
+}
+.btncad {
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-weight: bold;
+  font-style: normal;
+  font-family: "Roboto", sans-serif;
+  color: #fff;
+  background: #ff691e;
+  text-transform: uppercase;
+  cursor: pointer;
+  outline: 0;
+  box-sizing: border-box;
+  transition-property: all;
+  transition-duration: 0.3s;
+  text-decoration: none;
+  font-size: 20px;
+  margin: 8% 0 0 45%;
+  width: 150px;
+  height: 50px;
+}
+.btncad:hover {
+  background: #ff691e;
+  opacity: 80%;
+  color: white;
+}
+.img {
+  filter: drop-shadow(0px 5px 10px rgba(15, 52, 6, 0.42));
+  height: 820px;
+  border-radius: 5px 0px 0px 5px;
+}
+.spinb {
+  margin: 0 0 0 12%;
+}
+.Comentarios {
+  position: absolute;
+  width: 198vh;
+  height: 105vh;
+  background: #ffffff;
+  box-shadow: 6px 8px 8px 9px #0f3406;
+  border-radius: 4px;
+  margin: 1% 0% 0 0%;
+}
+.TituloComentario {
+  font-family: "Jomolhari";
+  margin: 4% 0 0 5%;
+}
+.FazerComentario {
+  display: flex;
+  justify-content: center;
+}
+.TextoComentario {
+  margin: 2% 0 0 4%;
+  width: 1950px;
+  height: 97px;
+  background: rgba(226, 255, 238, 0.95);
+  box-shadow: 0px 4px 4px rgba(35, 184, 94, 0.63);
+  margin-bottom: 0 0% 0 5%;
+}
+.BotaoComentario {
+  width: 300px;
+  height: 50px;
+  background: rgba(62, 150, 97, 0.95);
+  border-radius: 5px;
+  margin: 2% 25% 0 3%;
+  font-size: 1.7rem;
+  font-weight: 500;
+  padding: 5px;
+  border-color: rgba(62, 150, 97, 0.95);
+}
+.BotaoComentario:hover {
+  opacity: 80%;
+  background: rgba(62, 150, 97, 0.95);
+  border-color: rgba(62, 150, 97, 0.95);
+}
+.Respostas {
+  display: flex;
+  margin: 5% 0 0 4%;
+}
+.Respostas2 {
+  display: flex;
+  margin: 1% 0 0 0;
+}
+.RespostasCliente {
+  width: 120vh;
+  height: 119px;
+  background: rgba(228, 255, 239, 0.95);
+  border-radius: 5px;
+  margin: 0 0% 0 3%;
+}
+.aResposta {
+  font-family: "Jomolhari";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 19px;
+  line-height: 24px;
+  text-align: justify;
+  padding: 25px;
+  color: #000000;
+}
+.BotaoComentario2 {
+  width: 150px;
+  height: 50px;
+  background: #efa058;
+  border-radius: 3px;
+  margin: 0% 10% 0 1%;
+  font-size: 1.5rem;
+  font-weight: 500;
+  padding: 5px;
+  border-color: #efa058;
+}
+.BotaoComentario2:hover {
+  background: #ff691e;
+  color: white;
+  border-color: #ff691e;
+}
+.aResposta2 {
+  font-family: "Jomolhari";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-align: justify;
+  color: #4d4949;
+  margin: 1% 0 0 11%;
+}
+.avatar {
+  margin: 1% 0 0 11%;
+}
+.aResposta3 {
+  font-family: "Jomolhari";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 24px;
+  text-align: justify;
+  padding: 25px;
+  color: #000000;
+  margin: 0 0 0 2%;
+}
+.icon {
+  margin: 2% 0 0 1%;
+}
+
+.hrlay {
+  margin: 0 0 0 0;
+}
+#footer {
+  margin: 5% 0 0 25%;
+  padding: 10px;
+}
+
+@media (max-width: 390px) {
+  .row {
+    border: 0;
+    width: 390px;
+  }
+  .col1 {
+    width: 390px;
+    box-shadow: none;
+    border-style: solid none solid none;
+    border-color: #0f3406;
+  }
+  .navpr {
+    box-shadow: none;
+  }
+  .produto {
+    padding: 0;
+    height: 1150px;
+  }
+  .img {
+    width: 390px;
+    height: 390px;
+  }
+  .col2 {
+    width: 390px;
+    box-shadow: none;
+    height: 100px;
+    margin: 0;
+    padding: 0;
+  }
+  .dprincipal {
+    width: 390px;
+  }
+  .nome {
+    margin: 15% 0 0 0;
+    text-align: center;
+  }
+  .btncad {
+    margin: 9% 0 0 35%;
+  }
+  #footer {
+    color: #3e9661;
+    font-size: 16px;
+    margin: 10% 10% 10% 10%;
+  }
+  .footer1 {
+    margin: 5%;
+    width: 270px;
+    padding: 5%;
+  }
+  .footer2 {
+    margin: 5%;
+    width: 270px;
+    padding: 5%;
+  }
+  .container {
+    width: 0%;
+  }
+}
+</style>
